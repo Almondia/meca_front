e21f8a2d49771587ac750c742add0d02db116e3b
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-03-02 02:16:08 +0900

    feat: Add common styles

---
+++ styles/constants.ts
@@ -0,0 +1,45 @@
+export const COLOR = {
+  bgDark: '#121212',
+  bgLight: '#FFFFFF',
+  brand1: '#20639B',
+  brand2: '#508EC0',
+  brand3: '#98C0E1',
+  brand4: '#D2EAEA',
+  txtLight: '#F1F1F1',
+  txtDark: '#212529',
+  success: '#12B886',
+  error: '#FF6B6B',
+  warning: '#FD7E40',
+  gray50: '#F5F5F5',
+  gray100: '#E8E8E8',
+  gray200: '#D8D9D9',
+  gray400: '#B0B0B0',
+  gray600: '#808080',
+  gray800: '#505050',
+};
+
+export const MEDIA = {
+  mobile: '(max-width: 576px)',
+  tablet: '(max-width: 992px)',
+};
+
+export const FONT_SIZE = {
+  huge: '1.5rem',
+  large: '1.125rem',
+  main: '1rem',
+  sub: '0.875rem',
+  caption: '0.75rem',
+  overline: '0.625rem',
+};
+
+export const SHADOW = {
+  normalLight: 'drop-shadow(0px 0px 6px rgba(0, 0, 0, 0.25))',
+  normalDark: 'drop-shadow(0px 0px 8px rgba(255, 255, 255, 0.45))',
+  boldLight: 'drop-shadow(0px 4px 10px 4px rgba(0, 0, 0, 0.27))',
+  boldDark: 'drop-shadow(0px 4px 10px 4px rgba(255, 255, 255, 0.45))',
+};
+
+export const BORDER = {
+  card: '6px',
+  button: '8px',
+};
