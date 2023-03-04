779b26b422b3449798c4c7f7df82e14409267f93
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-03-04 13:44:10 +0900

    chore: storybook 기본 설정 추가

---
+++ .storybook/main.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-docs',
+    '@storybook/addon-actions',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json'),
+      }),
+    ];
+    config.module.rules.unshift({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
---
+++ .storybook/preview.tsx
@@ -0,0 +1,84 @@
+import * as NextImage from 'next/image';
+
+import { RecoilRoot, useSetRecoilState } from 'recoil';
+import ThemeProvider from '../styles/ThemeProvider';
+import themeState from '../atoms/common';
+import { DecoratorFn } from '@storybook/react';
+import React from 'react';
+
+const customViewports = {
+  mobile: {
+    name: 'Mobile',
+    styles: {
+      width: '360px',
+      height: '100%',
+    },
+  },
+  tablet: {
+    name: 'Tablet',
+    styles: {
+      width: '768px',
+      height: '100%',
+    },
+  },
+  pc: {
+    name: 'Desktop',
+    styles: {
+      width: '1440px',
+      height: '100%',
+    },
+  },
+};
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  viewport: { viewports: customViewports },
+  layout: 'fullscreen',
+};
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'circlehollow',
+      items: ['light', 'dark'],
+      showName: true,
+      dynamicTitle: true,
+    },
+  },
+};
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
+
+const ThemeChanger = ({ theme }) => {
+  const setThemeStatus = useSetRecoilState(themeState);
+  setThemeStatus(theme);
+  return <></>;
+};
+
+const withDecorator: DecoratorFn = (StoryFn, context) => {
+  const theme = context.parameters.theme || context.globals.theme;
+  return (
+    <RecoilRoot>
+      <ThemeChanger theme={theme} />
+      <ThemeProvider>
+        <StoryFn />
+      </ThemeProvider>
+    </RecoilRoot>
+  );
+};
+
+export const decorators = [withDecorator];
