c23cd0d645aef0ae5e9c5a52ebeab914ae27dcad
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-03-08 20:43:03 +0900

    feat: OAuth LoginCard 컴포넌트 추가

---
+++ components/organisms/LoginCard/styled.ts
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+import { FlexColumnCenter } from '@/styles/layout';
+
+export const LoginCardWrapper = styled.div`
+  ${FlexColumnCenter};
+  row-gap: 14px;
+  width: 358px;
+  height: 400px;
+`;
+
+export const SocialButtonContainer = styled.div`
+  ${FlexColumnCenter};
+  row-gap: 4px;
+`;
+
+const SocialButton = styled.button`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  width: 210px;
+  height: 42px;
+  border-radius: 4px;
+`;
+
+export const KakaoButton = styled(SocialButton)`
+  color: #3a2929;
+  background-color: #fddc3f;
+`;
+
+export const NaverButton = styled(SocialButton)`
+  color: white;
+  background-color: #00c73c;
+`;
+
+export const GoogleButton = styled(SocialButton)`
+  border: 1px solid var(--color-gray400);
+  background-color: white;
+  & > div {
+    padding-top: 5px;
+  }
+`;
