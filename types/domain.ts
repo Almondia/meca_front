3bf31bb8ef199140da2735e24f93ff5986f359e6
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-03-09 20:02:18 +0900

    feat: social type type 추가

--- types/domain.ts
+++ types/domain.ts
@@ -1,1 +1,4 @@
 export type MecaTagType = 'ox' | 'desc' | 'keyword' | 'select';
+
+export const SOCIAL_TYPES = ['kakao', 'naver', 'google'] as const;
+export type SocialType = (typeof SOCIAL_TYPES)[number];
