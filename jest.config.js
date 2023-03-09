b490cfc3621b8745083f670b9e09445b1e56e68c
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-03-09 19:26:16 +0900

    chore: jest-styled-component를 jest setupFiles에 추가

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,20 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const nextJest = require("next/jest");
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: "./",
+  dir: './',
 });
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  testRegex: "\\.test\\.[jt]sx?$",
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js', 'jest-styled-components'],
+  testRegex: '\\.test\\.[jt]sx?$',
   moduleNameMapper: {
     // Handle module aliases (this will be automatically configured for you soon)
-    "^@/(.*)$": "<rootDir>/$1",
+    '^@/(.*)$': '<rootDir>/$1',
   },
-  testEnvironment: "jest-environment-jsdom",
+  testEnvironment: 'jest-environment-jsdom',
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
