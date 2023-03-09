ce63bac38bdfa9b394e373a203a8dd498058785b
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-03-09 23:40:32 +0900

    test: jest render에 ToastContainer 추가

--- __tests__/utils.js
+++ __tests__/utils.js
@@ -2,11 +2,17 @@ import 'jest-styled-components';
 import { ThemeProvider } from 'styled-components';
 import { render } from '@testing-library/react';
 import themeMode from '../styles/theme';
+import { ToastContainer } from 'react-toastify';
 
-const Wrapper = ({ children }) => <ThemeProvider theme={themeMode['light']}>{children}</ThemeProvider>;
+const Wrapper = ({ children }) => (
+  <ThemeProvider theme={themeMode['light']}>
+    <ToastContainer />
+    {children}
+  </ThemeProvider>
+);
 
-const renderWithStyledComponent = (ui, options) => render(ui, { wrapper: Wrapper, ...options });
+const renderWithUIComponents = (ui, options) => render(ui, { wrapper: Wrapper, ...options });
 
 export * from '@testing-library/react';
 
-export { renderWithStyledComponent as render };
+export { renderWithUIComponents as render };
