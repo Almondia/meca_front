0375ea17e3fb6884a83c0341eb31e9676111a067
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-03-12 02:18:12 +0900

    feat: navigation 컴포넌트에 회원 상태 props 추가

--- components/organisms/Navigation/index.tsx
+++ components/organisms/Navigation/index.tsx
@@ -1,25 +1,42 @@
-import { useRecoilState, useRecoilValue } from 'recoil';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import Image from 'next/image';
+import Link from 'next/link';
 
-import themeState from '@/atoms/common';
 import IconButton from '@/components/atoms/IconButton';
 import useCustomTheme from '@/hooks/useCustomTheme';
 
-import { Logo, NavBar, NavigationWrapper } from './styled';
+import { Logo, NavBar, NavigationWrapper, Profile } from './styled';
 
-const Navigation = () => {
-  const theme = useRecoilValue(themeState);
-  const { toggleTheme } = useCustomTheme();
+export interface NavigationProps {
+  profileImage?: string;
+  loginUserName?: string;
+}
 
+const Navigation = ({ loginUserName, profileImage }: NavigationProps) => {
+  const { theme, toggleTheme } = useCustomTheme();
   return (
     <NavigationWrapper>
       {/* TODO: add LOGO */}
       <Logo>LOGO</Logo>
       <NavBar>
         <div>
-          <IconButton icon={theme === 'light' ? 'Lightmode' : 'Darkmode'} onClick={toggleTheme} />
+          <IconButton icon={theme === 'light' ? 'Lightmode' : 'Darkmode'} iconSize="20px" onClick={toggleTheme} />
         </div>
-        {/* TODO: add Profile Components */}
-        <div>my profile</div>
+        {loginUserName ? (
+          <Profile>
+            <Image
+              src={profileImage ?? '/images/noprofile.png'}
+              alt={profileImage ?? 'profile-image'}
+              width={30}
+              height={30}
+            />
+            <p>{loginUserName}</p>
+          </Profile>
+        ) : (
+          <div>
+            <Link href="">LOGIN</Link>
+          </div>
+        )}
       </NavBar>
     </NavigationWrapper>
   );
--- components/organisms/Navigation/styled.ts
+++ components/organisms/Navigation/styled.ts
@@ -1,25 +1,41 @@
 import styled from 'styled-components';
 
 import { FlexCenter, FlexSpaceBetween } from '@/styles/layout';
-import { COLOR } from '@/styles/constants';
 
 export const NavigationWrapper = styled.header`
   ${FlexSpaceBetween};
   align-items: center;
   padding: 4px 90px;
-  background-color: ${COLOR.brand3};
+  background-color: rgba(128, 171, 201, 0.3);
   @media ${({ theme }) => theme.media.tablet} {
     padding: 4px 30px;
+  }
+  @media ${({ theme }) => theme.media.mobile} {
+    padding: 4px 15px;
   }
 `;
 
 export const Logo = styled.div`
   font-size: ${({ theme }) => theme.fontSize.large};
   font-weight: ${({ theme }) => theme.fontWeight.bold};
-  color: ${COLOR.brand1};
+  color: ${({ theme }) => theme.brandColor};
 `;
 
 export const NavBar = styled.nav`
   ${FlexCenter};
   column-gap: 12px;
+  @media ${({ theme }) => theme.media.mobile} {
+    column-gap: 4px;
+  }
+`;
+
+export const Profile = styled.div`
+  ${FlexCenter};
+  column-gap: 6px;
+  cursor: pointer;
+  @media ${({ theme }) => theme.media.mobile} {
+    & > p {
+      display: none;
+    }
+  }
 `;
