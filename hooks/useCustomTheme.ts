e6198eb33553ad0745177114e0c06552b0214147
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-03-11 23:17:49 +0900

    feat: useDetactToken hook 추가

--- hooks/useCustomTheme.ts
+++ hooks/useCustomTheme.ts
@@ -1,7 +1,7 @@
 import { useCallback } from 'react';
 import { useRecoilState } from 'recoil';
 
-import themeState, { ThemeType } from '@/atoms/common';
+import { themeState, ThemeType } from '@/atoms/common';
 import storage from '@/utils/storageHandler';
 
 const useCustomTheme = () => {
