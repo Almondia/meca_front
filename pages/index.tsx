103ea786cac4db31f3228e432d18aa2b6412d665
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-03-12 16:35:43 +0900

    feat: home page ui 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,32 @@
+import styled from 'styled-components';
+
+import LoginCard from '@/components/organisms/LoginCard';
+import HomeMainSection from '@/components/layout/HomeMainSection';
+import { FlexColumn } from '@/styles/layout';
+
+const IntroduceContainer = styled.div`
+  ${FlexColumn};
+  row-gap: 30px;
+  max-width: 540px;
+  @media ${({ theme }) => theme.media.tablet} {
+    text-align: center;
+  }
+`;
+
+const LoginCardContainer = styled.div`
+  box-shadow: ${({ theme }) => theme.shadow.normal};
+`;
+
 export default function Home() {
   return (
-    <div>
-      <h2>hello</h2>
-    </div>
+    <HomeMainSection>
+      <IntroduceContainer>
+        <h2>Meca 서비스 간단 설명</h2>
+        <h4>조금 더 긴 서비스 설명을 설명해보고 설명하고 설명해보아요 배경을 꾸며요</h4>
+      </IntroduceContainer>
+      <LoginCardContainer>
+        <LoginCard />
+      </LoginCardContainer>
+    </HomeMainSection>
   );
 }
