074a8b46694ef412369385a116b445091cfc9ffa
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-03-13 19:56:42 +0900

    feat: snake => camel convert 로직 response interceptor에 추가

--- apis/config/instance.ts
+++ apis/config/instance.ts
@@ -1,28 +1,35 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 import storage from '@/utils/storageHandler';
+import { TokenType } from '@/types/domain';
+import snakeToCamel from '@/utils/snakeToCamel';
 
 const baseURL = process.env.NEXT_PUBLIC_API_URL;
 const unauthInstance = axios.create({ baseURL });
 const authInstance = axios.create({ baseURL });
 
+function getObject(response: AxiosResponse) {
+  const { data } = response;
+  return data !== null && typeof data === 'object' ? snakeToCamel(response.data) : {};
+}
+
 authInstance.interceptors.request.use((config) => {
   // eslint-disable-next-line no-param-reassign
-  config.headers.Authorization = `Bearer ${storage.getItem('accessToken')}`;
+  config.headers.Authorization = `Bearer ${storage.getItem<TokenType>('token')?.accessToken}`;
   return config;
 });
 
 unauthInstance.interceptors.response.use(
-  (response) => response.data,
+  (response): any => getObject(response),
   (error) => Promise.reject(error),
 );
 
 authInstance.interceptors.response.use(
-  (response) => response.data,
+  (response): any => getObject(response),
   (error) => {
     // TODO add token refresh logic
     if (error.status === 401) {
-      storage.removeItem('accessToken');
+      storage.removeItem('token');
     }
     return Promise.reject(error);
   },
