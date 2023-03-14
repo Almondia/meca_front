b0fe3564f9c29eb554607c4a1c4c9eb47e1ab467
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-03-14 12:47:20 +0900

    feat: CategoryControl 컴포넌트 추가

---
+++ components/organisms/CategoryControl/index.tsx
@@ -0,0 +1,38 @@
+import Button from '@/components/atoms/Button';
+import Input from '@/components/atoms/Input';
+import useInput from '@/hooks/useInput';
+
+import { CategoryControlComponentsContainer, CategoryControlWrapper } from './styled';
+
+/** 카테고리 목록 페이지 상단 컨트롤 레이아웃 */
+const CategoryControl = () => {
+  // TODO: 로직 추가
+  const { input: searchKeyword, onInputChange: handleSearchKeywordChange } = useInput('');
+  return (
+    <CategoryControlWrapper>
+      <CategoryControlComponentsContainer>
+        <Input.Text
+          iconLeft="Zoomin"
+          name="search"
+          value={searchKeyword}
+          onChange={handleSearchKeywordChange}
+          placeholder="제목+내용 카드 검색"
+        />
+        <Button colorTheme="primary" width="100px" onClick={() => console.log('HI')}>
+          검색
+        </Button>
+      </CategoryControlComponentsContainer>
+      <CategoryControlComponentsContainer>
+        <Button colorTheme="primary" onClick={() => console.log('HI')}>
+          추가하기 +
+        </Button>
+        <Button colorTheme="success" onClick={() => console.log('HI')}>
+          <Button.RightIcon icon="Play" />
+          <Button.InnerText>플레이</Button.InnerText>
+        </Button>
+      </CategoryControlComponentsContainer>
+    </CategoryControlWrapper>
+  );
+};
+
+export default CategoryControl;
---
+++ components/organisms/CategoryControl/styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+import { FlexSpaceBetween } from '@/styles/layout';
+
+export const CategoryControlWrapper = styled.div`
+  ${FlexSpaceBetween};
+`;
+
+export const CategoryControlComponentsContainer = styled.div`
+  display: flex;
+  column-gap: 10px;
+`;
