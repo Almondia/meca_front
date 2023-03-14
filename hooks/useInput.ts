67c4a8a488bc7095e85fba55b6f1212c8ef06640
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-03-14 15:46:30 +0900

    feat: useInput hook에 값 변경 여부 상태 추가

--- hooks/useInput.ts
+++ hooks/useInput.ts
@@ -2,16 +2,19 @@ import { useState } from 'react';
 
 function useInput(initialInput: string) {
   const [input, setInput] = useState(initialInput);
+  const [changed, setChange] = useState<boolean>(false);
 
   const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value.trim());
+    !changed && setChange(true);
   };
 
   const inputReset = () => {
     setInput('');
+    setChange(false);
   };
 
-  return { input, setInput, onInputChange, inputReset };
+  return { input, setInput, onInputChange, inputReset, changed };
 }
 
 export default useInput;
