0ba5fbc440d1c4a40cbaabc33af815e9e91fbfa0
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-03-21 11:34:37 +0900

    feat: 로그인 페이지에서의 처리를 ssr로 변경하고 토큰 정보를 쿠키에 담도록 변경

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,36 +1,61 @@
 import { useRouter } from 'next/router';
 
 import { useEffect } from 'react';
+import { GetServerSideProps } from 'next';
+import nookies from 'nookies';
 
 import { SOCIAL_TYPES, SocialType } from '@/types/domain';
 import parseQueryString from '@/utils/queryStringHandler';
 import userApi from '@/apis/userApi';
-import storage from '@/utils/storageHandler';
 import alertToast from '@/utils/toastHandler';
 
 const isSocialType = (value: any): value is SocialType => SOCIAL_TYPES.includes(value);
 
-const Login = () => {
+export interface LoginPageProps {
+  message: string;
+}
+
+const Login = ({ message }: LoginPageProps) => {
   const router = useRouter();
   useEffect(() => {
-    const { asPath } = router;
-    const { auth, code } = parseQueryString(asPath);
-    if (!isSocialType(auth) || !code) {
-      router.replace('/');
-      return;
-    }
-    (async () => {
-      const tokenResponse = await userApi[`${auth}Login`](code);
-      storage.setItem('token', tokenResponse);
-      alertToast('로그인 성공', 'success');
-      router.replace('/');
-    })();
+    alertToast(message, 'info');
+    router.replace('/');
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [message]);
   return (
     // TODO: 로딩 화면 구성할 것
     <div>please wait...</div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { url } = context.req;
+  const { res } = context;
+  const { auth, code } = parseQueryString(url);
+  if (!isSocialType(auth) || !code) {
+    res.writeHead(302, { Location: '/' });
+    res.end();
+    return {
+      props: {},
+    };
+  }
+  let message = '로그인 성공';
+  try {
+    const tokenResponse = await userApi[`${auth}Login`](code);
+    nookies.set(context, 'accessToken', tokenResponse.accessToken, {
+      maxAge: 6 * 60 * 60,
+      path: '/',
+      httpOnly: true,
+      secure: true,
+    });
+  } catch (e) {
+    message = (e as Error).message ?? '로그인 실패';
+  }
+  return {
+    props: {
+      message,
+    },
+  };
 };
 
 export default Login;
