27693fa6624b3c2e4fb624f0188f840753b766b2
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-03-21 20:58:23 +0900

    chore: 불필요 파일 삭제

--- atoms/common.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import { atom } from 'recoil';
-
-import storage from '@/utils/storageHandler';
-
-export const hasTokenState = atom<boolean>({
-  key: 'hasTokenState',
-  default: storage.getItem('token'),
-});
