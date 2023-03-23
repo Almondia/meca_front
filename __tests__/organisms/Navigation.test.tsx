4592c408a8c47d3b541f10a457d9d3e6e029b203
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-03-23 12:11:32 +0900

    test: 변경 사항 jest test 코드 반영

--- __tests__/organisms/Navigation.test.tsx
+++ __tests__/organisms/Navigation.test.tsx
@@ -1,19 +1,24 @@
 import Navigation from '@/components/organisms/Navigation';
-import { render } from '../utils';
+import { RecoilObserver, renderQuery } from '../utils';
 import { screen, fireEvent } from '@testing-library/react';
+import { hasAuthState } from '@/atoms/common';
 
 describe('Navigation', () => {
   it('비로그인 사용자일 경우 navigation에 login 링크가 식별된다.', () => {
-    render(<Navigation />);
+    renderQuery(<Navigation />);
     const linkText = screen.getByRole('link', {
       name: 'LOGIN',
     });
     expect(linkText).toBeInTheDocument();
   });
 
-  it('로그인 사용자일 경우 navigation에 프로필이 식별된다.', () => {
-    render(<Navigation loginUserName="user-name" profileImage="/abc/profile-image.jpg" />);
-    const imageAltText = screen.getByAltText('/abc/profile-image.jpg');
+  it('로그인 사용자일 경우 navigation에 프로필이 식별된다.', async () => {
+    renderQuery(
+      <>
+        <Navigation />
+      </>,
+    );
+    const imageAltText = await screen.findByAltText('/user.jpg');
     expect(imageAltText).toBeInTheDocument();
   });
 });
