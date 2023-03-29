2012938bf33fd97272c32bab2c6a871f5527abd8
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-03-29 19:58:22 +0900

    feat: QuizCounter 컴포넌트 추가

---
+++ components/atoms/QuizCounter/index.tsx
@@ -0,0 +1,18 @@
+import { QuizCounterWrapper, QuizCurrentCounter, QuizMaxCounter } from './styled';
+
+export interface QuizCounterProps {
+  currentCount: number;
+  maxCount: number;
+}
+
+const QuizCounter = ({ currentCount, maxCount }: QuizCounterProps) => (
+  <QuizCounterWrapper>
+    <QuizCurrentCounter isMoreThanHalf={currentCount / maxCount >= 0.5}>
+      {Math.min(currentCount, maxCount)}
+    </QuizCurrentCounter>
+    <QuizMaxCounter>/</QuizMaxCounter>
+    <QuizMaxCounter>{maxCount}</QuizMaxCounter>
+  </QuizCounterWrapper>
+);
+
+export default QuizCounter;
