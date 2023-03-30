3a7cbc9935ef9d01c90403f5ef8cfed14c7248c6
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-03-30 15:22:46 +0900

    feat: QuizBox 컴포넌트 추가

---
+++ components/atoms/QuizBox/index.tsx
@@ -0,0 +1,16 @@
+import { QuizBoxBody, QuizBoxHeader, QuizBoxWrapper } from './styled';
+
+export interface QuizBoxProps {
+  header: string;
+  body: string;
+  isColumn?: boolean;
+}
+
+const QuizBox = ({ body, header, isColumn }: QuizBoxProps) => (
+  <QuizBoxWrapper isColumn={isColumn ?? false}>
+    <QuizBoxHeader>{header}</QuizBoxHeader>
+    <QuizBoxBody>{body}</QuizBoxBody>
+  </QuizBoxWrapper>
+);
+
+export default QuizBox;
