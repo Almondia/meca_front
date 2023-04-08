cbc19e0b6fbadc65291ba3a7d3d21ad8ce5aa0a8
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-04-08 19:58:58 +0900

    refactor: Category 등록, 업데이트 Dialog 통합

--- components/molcules/CategoryUpdateDialog/index.tsx
+++ components/molcules/CategoryUpdateDialog/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import useCategoryPost from '@/hooks/category/useCategoryPost';
 import useCategoryUpdate from '@/hooks/category/useCategoryUpdate';
 import useInput from '@/hooks/useInput';
 import { DefaultModalOptions } from '@/types/common';
@@ -8,38 +9,38 @@ import InputGroup from '../InputGroup';
 import Modal from '../Modal';
 
 export interface CategoryUpdateDialogProps extends DefaultModalOptions {
-  categoryId: string;
+  categoryId?: string;
   categoryTitle: string;
 }
 
 const CategoryUpdateDialog = ({ visible, onClose, categoryId, categoryTitle }: CategoryUpdateDialogProps) => {
   const { input: title, onInputChange: onTitleChange } = useInput(categoryTitle);
   const { updateCategory } = useCategoryUpdate(onClose);
+  const { addCategory } = useCategoryPost();
   const handleUpdateClick = () => {
     if (title === '' || title === categoryTitle) {
       return;
     }
-    updateCategory({
-      categoryId,
-      title,
-    });
+    categoryId ? updateCategory({ categoryId, title }) : addCategory({ title });
+    onClose();
   };
+  const keyword = categoryId ? '수정' : '추가';
   return (
     <Modal visible={visible} onClose={onClose} hasCloseIcon={false}>
-      <Modal.Title>카테고리 수정하기</Modal.Title>
+      <Modal.Title>카테고리 {keyword}하기</Modal.Title>
       <Modal.Body>
         <InputGroup>
-          <InputGroup.Label>제목 수정하기</InputGroup.Label>
+          <InputGroup.Label>제목 {keyword}하기</InputGroup.Label>
           <InputGroup.Input.Text
-            name="category-update"
+            name="category-title"
             value={title}
             onChange={onTitleChange}
             placeholder="카테고리 제목 입력"
-            ariaLabel="input-category-update"
+            ariaLabel="input-category-title"
           />
         </InputGroup>
       </Modal.Body>
-      <Modal.ConfirmButton onClick={handleUpdateClick}>수정하기</Modal.ConfirmButton>
+      <Modal.ConfirmButton onClick={handleUpdateClick}>{categoryId ? '수정하기' : '등록하기'}</Modal.ConfirmButton>
       <Modal.CloseButton onClick={onClose}>취소</Modal.CloseButton>
     </Modal>
   );
--- components/organisms/CategoryControl/index.tsx
+++ components/organisms/CategoryControl/index.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 import Button from '@/components/atoms/Button';
 import Input from '@/components/atoms/Input';
-import CategoryAddButtonDialog from '@/components/molcules/CategoryAddButtonDialog';
+import CategoryUpdateDialog from '@/components/molcules/CategoryUpdateDialog';
 import useInput from '@/hooks/useInput';
+import useModal from '@/hooks/useModal';
 
 import { CategoryControlComponentsContainer, CategoryControlWrapper } from './styled';
 
 /** 카테고리 목록 페이지 상단 컨트롤 레이아웃 */
 const CategoryControl = ({ onChangeQuery }: { onChangeQuery: (query: string) => void }) => {
   const { input: searchKeyword, onInputChange: handleSearchKeywordChange } = useInput('');
+  const { visible: addCategoryVisible, open: addCategoryOpen, close: addCategoryClose } = useModal();
   const handleSearchQuery = () => {
     if (searchKeyword.length >= 100) {
       return;
@@ -33,7 +35,12 @@ const CategoryControl = ({ onChangeQuery }: { onChangeQuery: (query: string) => 
         </Button>
       </CategoryControlComponentsContainer>
       <CategoryControlComponentsContainer>
-        <CategoryAddButtonDialog />
+        <Button colorTheme="primary" onClick={addCategoryOpen}>
+          추가하기 +
+        </Button>
+        {addCategoryVisible && (
+          <CategoryUpdateDialog categoryTitle="" visible={addCategoryVisible} onClose={addCategoryClose} />
+        )}
         <Button colorTheme="success" onClick={() => console.log('HI')}>
           <Button.RightIcon icon="Play" />
           <Button.InnerText>랜덤 플레이</Button.InnerText>
