e86734ea736ad93a30da56a5d42ea15f93b7dfa8
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-04-08 22:20:21 +0900

    feat: ThumbnailUploader 컴포넌트 추가

---
+++ components/molcules/ThumbnailUploader/styled.ts
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+
+import { FlexCenter, FlexColumnCenter } from '@/styles/layout';
+
+export const ThumbnailUploaderWrapper = styled.div`
+  position: relative;
+  ${FlexColumnCenter};
+  width: 140px;
+  height: 100px;
+  padding: 4px;
+  border-radius: ${({ theme }) => theme.border.card};
+  background-color: var(--color-lightgray);
+`;
+
+export const ThumbnailHiddenInputBox = styled.div`
+  position: absolute;
+  width: 0px;
+  height: 0px;
+  overflow: hidden;
+`;
+
+export const ThumbnailUploadButton = styled.button`
+  position: relative;
+  ${FlexColumnCenter};
+  row-gap: 6px;
+  width: 100%;
+  height: 100%;
+  border: 1px dashed var(--color-gray);
+  border-radius: 2px;
+  color: var(--color-text);
+`;
+
+export const ThumbnailDeleteButton = styled.button`
+  position: absolute;
+  right: 0;
+  top: 0;
+  min-width: 24px;
+  border-radius: 0;
+  border-top-right-radius: 2px;
+  color: white;
+  background-color: var(--color-gray800);
+  :hover {
+    background-color: var(--color-gray600);
+  }
+`;
+
+export const ThumbnailImageContainer = styled(ThumbnailUploadButton.withComponent('div'))<{ image: string }>`
+  background-image: url(${(props) => props.image});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover; /* or cover */
+`;
+
+export const ThumbnailChangeBox = styled.div`
+  ${FlexCenter};
+  justify-content: flex-end;
+  margin-top: 4px;
+  max-width: 140px;
+  column-gap: 10px;
+`;
+
+export const ThumbnailChangeLink = styled.a`
+  font-size: ${({ theme }) => theme.fontSize.caption};
+  cursor: pointer;
+  :hover {
+    text-decoration: underline;
+  }
+`;
