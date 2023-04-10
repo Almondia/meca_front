e3ceaa610d6ad872fd0ab3c838c562065335d5f5
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-04-10 17:24:54 +0900

    feat: useImage hook validation, getImageInfo 함수 추가

--- hooks/useImage.ts
+++ hooks/useImage.ts
@@ -1,18 +1,29 @@
 import { useCallback, useState } from 'react';
 
-import { IMAGE_EXTENTIONS } from '@/types/domain';
+import { IMAGE_EXTENTIONS, ImageUploadRequestType } from '@/types/domain';
 import alertToast from '@/utils/toastHandler';
 
 const useImage = (initalImage: string | undefined) => {
   const [image, setImage] = useState<string | File | undefined>(initalImage);
 
+  const validImageFile = (uploadImage?: File) => {
+    if (!uploadImage || uploadImage.type.indexOf('image/') === -1) {
+      return { valid: false, message: '올바른 이미지 파일을 등록하세요' };
+    }
+    if (!IMAGE_EXTENTIONS.includes(uploadImage.type.replace('image/', '') as (typeof IMAGE_EXTENTIONS)[number])) {
+      return { valid: false, message: 'jpg/jpeg/png/gif 확장자만 업로드 가능합니다.' };
+    }
+    if (uploadImage.name.split('.').length !== 2) {
+      return { valid: false, message: '경로를 제외한 이미지 파일 이름에 "." 이 포함되어있습니다.' };
+    }
+    return { valid: true, message: '' };
+  };
+
   const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     const uploadedImage = e.target?.files?.[0];
-    if (
-      uploadedImage?.type.indexOf('image/') === -1 ||
-      !IMAGE_EXTENTIONS.includes(uploadedImage?.type.replace('image/', '') as (typeof IMAGE_EXTENTIONS)[number])
-    ) {
-      alertToast('이미지 파일을 등록해주세요', 'warning');
+    const checkValidImage = validImageFile(uploadedImage);
+    if (!checkValidImage.valid) {
+      alertToast(checkValidImage.message, 'warning');
       e.target.files = null;
       return;
     }
@@ -20,10 +31,16 @@ const useImage = (initalImage: string | undefined) => {
   }, []);
 
   const onDelete = useCallback(() => {
-    setImage('');
+    setImage(undefined);
   }, []);
 
-  return { image, onChange, onDelete };
+  const getImageInfo = (uploadedImage: File): Omit<ImageUploadRequestType, 'purpose'> => {
+    const fileName = uploadedImage.name.split('.')[0];
+    const extension = uploadedImage.type.replace('image/', '') as (typeof IMAGE_EXTENTIONS)[number];
+    return { fileName, extension };
+  };
+
+  return { image, validImageFile, getImageInfo, onChange, onDelete };
 };
 
 export default useImage;
