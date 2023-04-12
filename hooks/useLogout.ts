eefa63337bb0994d8903bf324a741a695c1361ce
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-04-12 23:57:06 +0900

    refactor: logout 강제 새로고침에서 사용자 상태 만료로 로직 개선

--- hooks/useLogout.ts
+++ hooks/useLogout.ts
@@ -1,16 +1,26 @@
-import axios from 'axios';
+import { useRouter } from 'next/router';
+
 import { useCallback } from 'react';
 
-import { hasBrowser } from '@/utils/common';
+import { useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
+import { useSetRecoilState } from 'recoil';
+
+import { hasAuthState } from '@/atoms/common';
+import queryKey from '@/query/queryKey';
 
 const useLogout = () => {
+  const queryClient = useQueryClient();
+  const setHasAuth = useSetRecoilState(hasAuthState);
+  const router = useRouter();
   const logout = useCallback(async () => {
     const { data } = await axios.get('/api/logout');
     if (data.deleted) {
-      if (hasBrowser()) {
-        window.location.href = '/';
-      }
+      queryClient.setQueryData([queryKey.me], null);
+      setHasAuth(false);
+      router.push('/');
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return { logout };
