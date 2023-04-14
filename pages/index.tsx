236e244ce02c0febe4e05b30e9d23b63389ac0b4
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-04-14 14:15:12 +0900

    feat: Home 페이지 Carousel Section UI 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,11 @@
 /* eslint-disable react/button-has-type */
 import { GetServerSideProps } from 'next';
 
-import styled from 'styled-components';
-
-import HomeMainSection from '@/components/layout/HomeMainSection';
+import HomeCarousel from '@/components/organisms/HomeCarousel';
 import { ssrAspect } from '@/libs/renderAspect';
-import { FlexColumn } from '@/styles/layout';
-
-const IntroduceContainer = styled.div`
-  ${FlexColumn};
-  row-gap: 30px;
-  max-width: 540px;
-  @media ${({ theme }) => theme.media.tablet} {
-    text-align: center;
-  }
-`;
 
 export default function Home() {
-  return (
-    <HomeMainSection>
-      <IntroduceContainer>
-        <h2>Meca 서비스 간단 설명</h2>
-        <h4>조금 더 긴 서비스 설명을 설명해보고 설명하고 설명해보아요 배경을 꾸며요</h4>
-      </IntroduceContainer>
-    </HomeMainSection>
-  );
+  return <HomeCarousel />;
 }
 
 export const getServerSideProps: GetServerSideProps = ssrAspect();
