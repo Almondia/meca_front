677114b6b5e7c6548c264379ca65df25439d7b92
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-04-16 11:35:35 +0900

    feat: Main 페이지 공유 카테고리 목록 페이징 조회 ISR 반영

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,36 @@
-/* eslint-disable react/button-has-type */
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 
+import categoryApi from '@/apis/categoryApi';
+import CategoryControl from '@/components/organisms/CategoryControl';
+import CategoryList from '@/components/organisms/CategoryList';
 import HomeCarousel from '@/components/organisms/HomeCarousel';
-import { ssrAspect } from '@/libs/renderAspect';
+import useSharedCategory from '@/hooks/category/useSharedCategory';
+import { isrAspect } from '@/libs/renderAspect';
+import queryKey from '@/query/queryKey';
+import { Devide, ListSection } from '@/styles/layout';
 
 export default function Home() {
-  return <HomeCarousel />;
+  const { categories, hasNextPage, fetchNextPage } = useSharedCategory();
+  return (
+    <>
+      <HomeCarousel />
+      <ListSection>
+        {/* TODO: backend 구현 완료 시 query change 추가 */}
+        <CategoryControl onChangeQuery={(str) => console.log(str)} isShared />
+        <Devide />
+        <CategoryList categoryList={categories} hasNextPage={hasNextPage} fetchNextPage={fetchNextPage} />
+      </ListSection>
+    </>
+  );
 }
 
-export const getServerSideProps: GetServerSideProps = ssrAspect();
+export const getStaticProps: GetStaticProps = isrAspect(async (_, queryClient) => {
+  await queryClient.prefetchInfiniteQuery(
+    [queryKey.categories, 'shared'],
+    () => categoryApi.getSharedCategoryList({}),
+    {
+      getNextPageParam: (lastPage) => lastPage.hasNext,
+    },
+  );
+  return {};
+});
