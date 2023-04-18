cb087f6e9158eb72bc75f206db51e94966bae33e
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-04-18 09:41:06 +0900

    feat: category 목록 title search 기능 추가

--- hooks/category/useSharedCategory.ts
+++ hooks/category/useSharedCategory.ts
@@ -1,9 +1,13 @@
+import { useCallback, useState } from 'react';
+
 import { useInfiniteQuery } from '@tanstack/react-query';
 
 import categoryApi from '@/apis/categoryApi';
 import queryKey from '@/query/queryKey';
 
 const useSharedCategory = () => {
+  const [query, setQuery] = useState<string>('');
+
   const {
     data: categories,
     isLoading,
@@ -11,13 +15,13 @@ const useSharedCategory = () => {
     hasNextPage,
     fetchNextPage,
   } = useInfiniteQuery(
-    [queryKey.categories, 'shared'],
+    [queryKey.categories, 'shared', query],
     ({ pageParam }) => {
       const props = {
         hasNext: pageParam,
       };
       !pageParam && delete props.hasNext;
-      return categoryApi.getSharedCategoryList(props);
+      return categoryApi.getSharedCategoryList({ ...props, containTitle: query });
     },
     {
       enabled: true,
@@ -25,7 +29,18 @@ const useSharedCategory = () => {
     },
   );
 
-  return { categories, isLoading, isError, fetchNextPage, hasNextPage };
+  const changeSearchQuery = useCallback(
+    (newQuery: string) => {
+      if (query === newQuery) {
+        return;
+      }
+      setQuery(newQuery);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [query],
+  );
+
+  return { categories, isLoading, isError, fetchNextPage, hasNextPage, changeSearchQuery };
 };
 
 export default useSharedCategory;
