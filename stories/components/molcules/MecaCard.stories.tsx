332f2a9df0b7629486209adc0d9277b7d007958e
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-04-24 13:34:07 +0900

    test: Meca 컴포넌트 썸네일 추가 테스트 코드 반영

--- stories/components/molcules/MecaCard.stories.tsx
+++ stories/components/molcules/MecaCard.stories.tsx
@@ -24,4 +24,6 @@ Default.args = {
   question: '이러쿵 저러쿵 한 문제를 문제라고 하는데 그것은 너무너무 그것은 스토리북인데 그것은 말이지',
   tagType: 'keyword',
   isMine: true,
+  description:
+    "<p>161616<img src='https://my-meca.s3.ap-northeast-2.amazonaws.com/01879c33-ebf3-6056-952f-d6d831d4b0bb/card/1682306625453.png'></p><p><br></p><p>&lt;img src=\"hello\"/&gt;</p><p><br></p><p><img src='https://my-meca.s3.ap-northeast-2.amazonaws.com/01879c33-ebf3-6056-952f-d6d831d4b0bb/card/1682300937980.png'></p>",
 };
