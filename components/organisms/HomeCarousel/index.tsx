62b0591ceed3ccfcceda3907d0038be24b05a262
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-05-06 13:47:27 +0900

    feat: home 이미지 사이즈 조절

--- components/organisms/HomeCarousel/index.tsx
+++ components/organisms/HomeCarousel/index.tsx
@@ -2,6 +2,7 @@ import 'slick-carousel/slick/slick.css';
 
 import Image from 'next/image';
 
+import MainImage from '@/public/images/mainimg1.png';
 import { TextBody } from '@/styles/common';
 import { COLOR } from '@/styles/constants';
 
@@ -20,7 +21,7 @@ const HomeCarousel = () => (
             </TextBody>
           </div>
           <div>
-            <Image src="/images/mainimg1.png" alt="main-image1" fill loading="eager" />
+            <Image src={MainImage} alt="main-image1" priority width={735} height={232} />
           </div>
         </HomeCarouselContent>
       </HomeCarouselContentBackground>
@@ -34,7 +35,7 @@ const HomeCarousel = () => (
             </TextBody>
           </div>
           <div>
-            <Image src="/images/mainimg1.png" alt="main-image1" fill />
+            <Image src={MainImage} alt="main-image2" width={735} height={232} placeholder="blur" />
           </div>
         </HomeCarouselContent>
       </HomeCarouselContentBackground>
