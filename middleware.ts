493858e033609605400c181cd1d035e521cb7a24
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-05-19 20:07:24 +0900

    feat: middleware 본인 카드 조회 페이지 rewrite 처리 제거

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-import jwtDecode, { JwtPayload } from 'jwt-decode';
-
-import { extractCombinedUUID } from './utils/uuidHandler';
-
 const authorizedPaths = ['/write', '/quiz', '/mypage'];
-
-function getUserPayload(token: string) {
-  try {
-    const { id } = jwtDecode<JwtPayload & { id?: string }>(token);
-    return id;
-  } catch {
-    return undefined;
-  }
-}
-
-function isUserResource(token: string, pathname: string) {
-  const combinedUUId = pathname.split('/')[2];
-  const { uuid1: memberId, uuid2 } = extractCombinedUUID(combinedUUId);
-  if (memberId && getUserPayload(token) === memberId) {
-    return uuid2;
-  }
-  return undefined;
-}
 
 /*
   redirect() - Returns a NextResponse with a redirect set
@@ -36,15 +14,9 @@ export function middleware(request: NextRequest) {
   if (!accessToken && authorizedPaths.some((path) => request.nextUrl.pathname.indexOf(path) !== -1)) {
     return NextResponse.rewrite(new URL('/401', request.nextUrl.origin));
   }
-  if (request.nextUrl.pathname.startsWith('/mecas/')) {
-    const requestId = isUserResource(accessToken, request.nextUrl.pathname);
-    return requestId
-      ? NextResponse.rewrite(new URL(`/mecas/me/${requestId}`, request.nextUrl.origin))
-      : NextResponse.next();
-  }
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/mecas/:path*', '/:path*/write/:path*', '/quiz', '/mypage/:path*'],
+  matcher: ['/:path*/write/:path*', '/quiz', '/mypage/:path*'],
 };
