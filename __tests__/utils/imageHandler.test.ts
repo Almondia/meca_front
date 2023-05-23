9021c6700a765236e639dcaead71df3ccb18de82
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-05-23 15:48:12 +0900

    test: useImage, imageHandler test case 이동

--- __tests__/utils/imageHandler.test.ts
+++ __tests__/utils/imageHandler.test.ts
@@ -1,4 +1,4 @@
-import { extractAllImageSrc, extractFirstImageSrc } from '@/utils/imageHandler';
+import { extractAllImageSrc, extractFirstImageSrc, getImageInfo, validImageFile } from '@/utils/imageHandler';
 
 describe('imageHandler', () => {
   describe('extractAllImageSrc', () => {
@@ -41,6 +41,44 @@ describe('imageHandler', () => {
         "<p>161616<img src='https://my-meca.s3.ap-northeast-2.amazonaws.com/01879c33-ebf3-6056-952f-d6d831d4b0bb/card/1682300565757.png'></p><p><br></p><p>&lt;img src=\"hello\"/&gt;</p><p><br></p><p><img src='https://my-meca.s3.ap-northeast-2.amazonaws.com/01879c33-ebf3-6056-952f-d6d831d4b0bb/card/1682300937980.png'></p>";
       const result = extractFirstImageSrc(tags, 'https://my-meca.s3.ap1111111');
       expect(result).toBeUndefined();
+    });
+  });
+
+  describe('validImageFile', () => {
+    it.each([['jpg'], ['jpeg'], ['png'], ['gif']])(
+      '지정된 (%s) 이미지 파일에 대해 validation이 통과한다.',
+      (arg0: string) => {
+        const file = new File(['abc'], `file.${arg0}`, { type: `image/${arg0}` });
+        const { valid } = validImageFile(file);
+        expect(valid).toBeTruthy();
+      },
+    );
+
+    it('지정되지 않은 이미지 파일에 대해 validation이 실패한다.', () => {
+      const file = new File(['abc'], 'file.bmp', { type: 'image/bmp' });
+      const { valid } = validImageFile(file);
+      expect(valid).toBeFalsy();
+    });
+
+    it('지정되지 않은 파일에 대해 validation이 실패한다.', () => {
+      const file = new File(['abc'], 'file.txt', { type: 'text/plain' });
+      const { valid } = validImageFile(file);
+      expect(valid).toBeFalsy();
+    });
+
+    it('이미지 이름에 "."이 포함되어있다면 validation이 실패한다.', () => {
+      const file = new File(['abc'], 'img.img.jpeg', { type: 'image/jpeg' });
+      const { valid } = validImageFile(file);
+      expect(valid).toBeFalsy();
+    });
+  });
+
+  describe('getImageInfo', () => {
+    it('이미지 파일의 info를 얻을 수 있다.', () => {
+      const file = new File(['abc'], 'name.jpeg', { type: 'image/jpeg' });
+      const { fileName, extension } = getImageInfo(file);
+      expect(fileName).toEqual('name');
+      expect(extension).toEqual('jpeg');
     });
   });
 });
