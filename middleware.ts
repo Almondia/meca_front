4b981ead555ed2fc9cbdce3b37da1063e877193d
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-05-26 09:35:37 +0900

    feat: middleware meca 개인 page rewrite 처리 추가 및 테스트케이스 추가

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,28 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+import jwtDecode, { JwtPayload } from 'jwt-decode';
+
+import { extractCombinedUUID } from './utils/uuidHandler';
+
 const authorizedPaths = ['/write', '/quiz', '/mypage'];
+
+function getUserPayload(token: string) {
+  try {
+    const { id } = jwtDecode<JwtPayload & { id?: string }>(token);
+    return id;
+  } catch {
+    return undefined;
+  }
+}
+
+function getCurrentUserId(token: string, pathname: string) {
+  const combinedUUId = pathname.split('/')[2];
+  const { uuid1: memberId, uuid2 } = extractCombinedUUID(combinedUUId);
+  if (memberId && getUserPayload(token) === memberId) {
+    return uuid2;
+  }
+  return undefined;
+}
 
 /*
   redirect() - Returns a NextResponse with a redirect set
@@ -13,6 +35,12 @@ export function middleware(request: NextRequest) {
   const accessToken = cookies.get('accessToken')?.value ?? '';
   if (!accessToken && authorizedPaths.some((path) => request.nextUrl.pathname.indexOf(path) !== -1)) {
     return NextResponse.rewrite(new URL('/401', request.nextUrl.origin));
+  }
+  if (request.nextUrl.pathname.startsWith('/mecas/')) {
+    const requestId = getCurrentUserId(accessToken, request.nextUrl.pathname);
+    return requestId
+      ? NextResponse.rewrite(new URL(`/mecas/me/${requestId}`, request.nextUrl.origin))
+      : NextResponse.next();
   }
   return NextResponse.next();
 }
