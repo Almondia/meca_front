849ecf9753036c3498133c115ce50e0bc931cb36
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-06-04 10:37:02 +0900

    feat: 공유 카드 상세 조회 client side에서 조회 실패 시 revalidate 처리 추가

--- hooks/meca/useMeca.ts
+++ hooks/meca/useMeca.ts
@@ -1,3 +1,5 @@
+import { useRouter } from 'next/router';
+
 import { useQuery } from '@tanstack/react-query';
 import { useRecoilValue } from 'recoil';
 
@@ -8,11 +10,22 @@ import { MecaType, UserProfile } from '@/types/domain';
 
 const useMeca = (cardId: string, shared?: boolean) => {
   const hasAuth = useRecoilValue(hasAuthState);
+  const router = useRouter();
+
   const { data, isLoading, isSuccess, isError } = useQuery(
     [queryKey.meca, cardId],
     shared ? () => mecaApi.getSharedCardById(cardId) : () => mecaApi.getMyCardById(cardId),
     {
       enabled: !!cardId && (shared ? true : hasAuth),
+      onError: () => {
+        (async () => {
+          if (shared) {
+            const { default: utilApi } = await import('@/apis/utilApi');
+            utilApi.revalidate([router.asPath]);
+          }
+          router.push('/');
+        })();
+      },
     },
   );
   const meca: (typeof shared extends boolean ? MecaType & UserProfile : MecaType) | undefined = data;
