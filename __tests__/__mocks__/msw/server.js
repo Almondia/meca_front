c322149d242791acc2e5518f452451a0717d2a3d
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-06-06 14:12:44 +0900

    test: rest handler 분리 및 msw server use,resetHandler 처리 함수 추가

--- __tests__/__mocks__/msw/server.js
+++ __tests__/__mocks__/msw/server.js
@@ -1,6 +1,14 @@
 import { setupServer } from 'msw/node';
 
-import { handlers } from './handlers';
+import { handlers, restHandler } from './handlers';
 
 export const server = setupServer(...handlers);
 server.listen({ port: 4000 });
+
+export const implementServer = (restHandlers) => {
+  server.use(...restHandlers);
+};
+
+export const resetServer = (restHandlers) => {
+  server.resetHandlers(...restHandlers);
+};
