39685b72a5085a127474d351d3735da4db172cf5
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-06-06 18:15:08 +0900

    feat: Meca 수정 api 및 UI 변경

--- components/organisms/MecaWrite/MecaWriteForm/answer/KeywordAnswer.tsx
+++ components/organisms/MecaWrite/MecaWriteForm/answer/KeywordAnswer.tsx
@@ -4,18 +4,16 @@ import InputGroup from '@/components/molcules/InputGroup';
 
 import { MecaWriteFormInputProps } from '../type';
 
-const KeywordAnswer = ({ value, onChange, isDisabled }: MecaWriteFormInputProps) => (
+const KeywordAnswer = ({ value, onChange }: MecaWriteFormInputProps) => (
   <div>
     <InputGroup>
-      <InputGroup.Label>{isDisabled ? '정답' : '정답을 작성하세요'}</InputGroup.Label>
-      {!isDisabled && <InputGroup.Description descLists={['정답은 수정할 수 없으니 신중하게 작성하세요!']} />}
+      <InputGroup.Label>정답을 작성하세요</InputGroup.Label>
       <InputGroup.Input.Text
         name="meca-answer-keyword"
         value={value}
         onChange={onChange}
         placeholder=""
         ariaLabel="input-meca-keyword-answer"
-        disabled={isDisabled}
       />
     </InputGroup>
   </div>
