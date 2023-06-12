74f4b4d69011ab107ee73407cd3397fef6427560
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-06-12 14:22:24 +0900

    feat: NumberIncreaseToggle 스타일 수정

--- components/atoms/NumberIncreaseToggle/styled.ts
+++ components/atoms/NumberIncreaseToggle/styled.ts
@@ -5,18 +5,28 @@ import { FlexCenter } from '@/styles/layout';
 export const NumberIncreaseToggleWrapper = styled.div`
   display: inline-flex;
   align-items: center;
+  & > div {
+    display: inline-flex;
+    border: 1px solid var(--color-lightgray);
+  }
 `;
 
 export const NumberIncreaseToggleInnerContainer = styled.div`
   ${FlexCenter};
-  padding: 6px;
-  border: 1px solid var(--color-lightgray);
+  padding: 6px 8px;
+  border-left: 1px solid var(--color-lightgray);
+  border-right: 1px solid var(--color-lightgray);
   font-size: ${({ theme }) => theme.fontSize.caption};
   font-weight: ${({ theme }) => theme.fontWeight.bold};
   color: var(--color-brand);
 `;
 
 export const NumberIncreaseToggleButton = styled(NumberIncreaseToggleInnerContainer.withComponent('button'))`
+  border-radius: 0;
+  border: none;
   color: var(--color-text);
-  cursor: pointer;
+  :hover {
+    background-color: var(--color-lightgray);
+    opacity: 0.7;
+  }
 `;
