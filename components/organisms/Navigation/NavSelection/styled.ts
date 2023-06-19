8ba58824e05fa35caf152513f210acafff0405ca
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-06-19 20:31:53 +0900

    feat: Navigation Selection Item text left 정렬 추가

--- components/organisms/Navigation/NavSelection/styled.ts
+++ components/organisms/Navigation/NavSelection/styled.ts
@@ -7,7 +7,7 @@ export const NavSelectionWrapper = styled.div`
   z-index: 2;
   top: 45px;
   right: -5px;
-  width: 140px;
+  width: 120px;
   border-radius: 2px;
   box-shadow: var(--shadow-normal);
   background-color: var(--color-background);
@@ -15,8 +15,9 @@ export const NavSelectionWrapper = styled.div`
 
 export const NavSelectionItem = styled(Link)`
   display: block;
-  padding: 12px 16px;
+  padding: 12px 16px 12px 24px;
   font-size: ${({ theme }) => theme.fontSize.sub};
+  text-align: left;
   color: var(--color-text);
   :hover {
     background-color: var(--color-gray100);
