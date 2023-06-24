c242cf1a7b8e53edcdde05599e143e0f36c79120
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-06-24 20:04:54 +0900

    feat: useQuizKeyword hook 추가

---
+++ hooks/quiz/useQuizKeyword.ts
@@ -0,0 +1,38 @@
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+
+import statisticsApi, { KeywordResponse } from '@/apis/statisticsApi';
+import queryKey from '@/query/queryKey';
+import { QuizType } from '@/types/domain';
+
+const useQuizKeyword = () => {
+  const queryClient = useQueryClient();
+  const fallbackKeywordResult: KeywordResponse = {
+    keywords: {},
+  };
+  const {
+    data: quizPhaseKeywords = fallbackKeywordResult,
+    isLoading: isQuizPhaseKeywordsLoading,
+    refetch: fetchQuizPhaseKeywords,
+  } = useQuery(
+    [queryKey.keyword, 'quiz'],
+    async () => {
+      const quizList = queryClient.getQueryData<QuizType[]>([queryKey.quiz]);
+      if (!quizList) {
+        return fallbackKeywordResult;
+      }
+      const sentence = quizList.reduce((prev, cur) => `${prev} ${cur.title} ${cur.question} ${cur.answer}`, '');
+      const response = await statisticsApi.postKeywordBySentence(sentence);
+      return response;
+    },
+    {
+      enabled: false,
+      staleTime: 3,
+      cacheTime: 3,
+      retry: 1,
+    },
+  );
+
+  return { quizPhaseKeywords, fetchQuizPhaseKeywords, isQuizPhaseKeywordsLoading };
+};
+
+export default useQuizKeyword;
