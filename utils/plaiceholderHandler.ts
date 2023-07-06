4c1fc28cbd685415c67996d1f9b944eedd080e24
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-07-06 15:00:45 +0900

    chore: placeholder image 사이즈 수정

--- utils/plaiceholderHandler.ts
+++ utils/plaiceholderHandler.ts
@@ -5,7 +5,7 @@ import { createQueryString } from './queryStringHandler';
 const NEXT_ORIGIN = process.env.NEXT_ORIGIN ?? 'http://127.0.0.1:3000';
 
 export const getNextImageUrl = (imageSrc: string) =>
-  `${NEXT_ORIGIN}/_next/image${createQueryString({ url: imageSrc, w: '322', q: '75' })}`;
+  `${NEXT_ORIGIN}/_next/image${createQueryString({ url: imageSrc, w: '256', q: '75' })}`;
 
 export const getPlaceholderImage = async (src: string, size: number) => {
   try {
