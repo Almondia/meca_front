4d5ad8097bca5c89c810103036d850d6bca4ffcb
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-07-17 19:41:46 +0900

    feat: 개인 Category 목록 페이지 추천한 목록 조회 기능 추가

--- hooks/useQueryRouter.ts
+++ hooks/useQueryRouter.ts
@@ -1,32 +1,24 @@
 import { useRouter } from 'next/router';
 
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 
-import { parseQueryString } from '@/utils/queryStringHandler';
-
-const useQueryRouter = (initialValue?: {
-  [key: string]: string;
-}): [{ [key: string]: string }, (queryString: { [key: string]: string }) => void] => {
-  const [queries, setQueries] = useState<{ [key: string]: string }>(initialValue ?? {});
+const useQueryRouter = <T extends string>(initialValue: Partial<Record<T, string>>) => {
+  const [queries, setQueries] = useState<Partial<Record<T, string>>>(initialValue);
   const router = useRouter();
-
-  useEffect(() => {
-    const parsedQueries = parseQueryString(router.asPath);
-    setQueries(parsedQueries);
-  }, [router]);
 
   // TODO: shallow routing(serverside rendering skip) 가능할지?
   // refernces: [vercel/next.js/discussions/18072, bamdadsabbagh/next-replace-url]
-  const replace = useCallback(
-    (queryString: { [key: string]: string }) => {
+  const replaceWithQuery = useCallback(
+    (queryString: Partial<Record<T, string>>) => {
       router.replace({
         pathname: router.asPath.split('?')[0],
         query: queryString,
       });
+      setQueries(queryString);
     },
     [router],
   );
-  return [queries, replace];
+  return { ...queries, replaceWithQuery };
 };
 
 export default useQueryRouter;
