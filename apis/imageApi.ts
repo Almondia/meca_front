c359478d5284c95c15ae5f6162e920a84c995be2
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-07-27 12:20:56 +0900

    feat: blur image api 추가

--- apis/imageApi.ts
+++ apis/imageApi.ts
@@ -2,6 +2,15 @@ import { ImageUploadRequestType } from '@/types/domain';
 
 import { authInstance, serverInstance } from './config/instance';
 
+export interface BlurImageType {
+  blurDataURL: string;
+  img: {
+    src: string;
+    width: number;
+    height: number;
+  };
+}
+
 const imageApi = {
   getPresignedUrl: ({ extension, purpose, fileName }: ImageUploadRequestType) =>
     authInstance.get<never, { url: string; objectKey: string }>('/api/v1/presign/images/upload', {
@@ -25,6 +34,17 @@ const imageApi = {
         timeout: 30000,
       },
     ),
+  getBlurImage: async (url: string) => {
+    try {
+      const response = await serverInstance.get<never, BlurImageType>(
+        `${process.env.NEXT_ORIGIN ?? ''}/api/image/blur`,
+        { params: { url } },
+      );
+      return response;
+    } catch (e) {
+      return undefined;
+    }
+  },
 };
 
 export default imageApi;
