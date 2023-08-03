a027aa7ff5a78b328f93e20efe89d871e407d201
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-08-04 01:11:54 +0900

    feat: useFlatInfiniteQuery 추가
    InfiniteData<??> 분해 및 일부 state, option 커스텀

---
+++ query/hooks/useFlatInfiniteQuery.ts
@@ -0,0 +1,39 @@
+/* eslint-disable import/prefer-default-export */
+import { QueryFunction, QueryKey, useInfiniteQuery, UseInfiniteQueryOptions } from '@tanstack/react-query';
+
+import { CursorPaginationType } from '@/types/domain';
+
+interface QueryFnData extends CursorPaginationType {
+  contents: any[];
+  [key: string]: any;
+}
+
+export function useFlatInfiniteQuery<
+  TQueryFnData extends QueryFnData = QueryFnData,
+  TError = unknown,
+  TQueryKey extends QueryKey = QueryKey,
+>(
+  queryKey: TQueryKey,
+  queryFn: QueryFunction<TQueryFnData, TQueryKey>,
+  option?: UseInfiniteQueryOptions<TQueryFnData, TError, TQueryFnData, TQueryFnData, TQueryKey>,
+) {
+  const {
+    data,
+    isFetching,
+    isError,
+    fetchNextPage: originFetchNextPage,
+    ...rest
+  } = useInfiniteQuery(queryKey, queryFn, {
+    getNextPageParam: (lastPage) => lastPage.hasNext ?? undefined,
+    ...option,
+  });
+  const isEmpty = (data?.pages[0]?.contents.length ?? 0) === 0;
+  const flatted = data
+    ? data.pages
+        .flatMap((page) => page)
+        .reduce((prev, current) => ({ ...current, contents: prev.contents.concat(current.contents) }))
+    : ({ contents: [], hasNext: undefined, pageSize: 0 } as unknown as TQueryFnData);
+  const fetchNextPage =
+    !option?.enabled || isFetching || isError ? ((() => {}) as typeof originFetchNextPage) : originFetchNextPage;
+  return { data: flatted, isEmpty, fetchNextPage, isFetching, isError, ...rest };
+}
