808602ac54aeac4922de7a4cd35fe9e350e5542e
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-08-04 13:55:07 +0900

    remove: ListInfiniteScroller 컴포넌트 제거

--- components/molcules/ListInfiniteScroller/listType/GridList.tsx
+++
@@ -1,5 +0,0 @@
-import { GridListWrapper } from '../styled';
-
-const GridList = ({ children }: { children: React.ReactNode }) => <GridListWrapper>{children}</GridListWrapper>;
-
-export default GridList;
