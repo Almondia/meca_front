32db7ff98333dd5b8586e932060fc13ce873d586
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-08-06 19:27:18 +0900

    refactor: useMecaHistory

--- hooks/meca/useMecaHistory.ts
+++ hooks/meca/useMecaHistory.ts
@@ -1,3 +1,5 @@
+import { QueryClient } from '@tanstack/react-query';
+
 import cardHistoryApi from '@/apis/cardHistoryApi';
 import { useFlatInfiniteQuery } from '@/query/hooks/useFlatInfiniteQuery';
 import queryKey from '@/query/queryKey';
@@ -19,5 +21,10 @@ const useMecaHistory = (resourceType: 'cards' | 'members', id: string) => {
   );
   return { cardHistoryList, isEmpty, isFetching, hasNextPage, fetchNextPage };
 };
+
+useMecaHistory.prefetchInfiniteQuery = (resourceType: 'cards' | 'members', id: string, queryClient: QueryClient) =>
+  queryClient.prefetchInfiniteQuery([queryKey.history, id], () => cardHistoryApi.getHistories({ id, resourceType }), {
+    getNextPageParam: (lastPage) => lastPage.hasNext ?? undefined,
+  });
 
 export default useMecaHistory;
--- pages/user/me/[memberId].tsx
+++ pages/user/me/[memberId].tsx
@@ -1,20 +1,21 @@
 /* eslint-disable @typescript-eslint/no-throw-literal */
 import { GetServerSideProps } from 'next';
+import dynamic from 'next/dynamic';
 
-import cardHistoryApi from '@/apis/cardHistoryApi';
 import AuthPageProvider from '@/components/common/AuthPageProvider';
 import MetaHead from '@/components/common/MetaHead';
 import RelativeDateText from '@/components/common/RelativeDateText';
 import PostBody from '@/components/molcules/PostBody';
 import PostSubInfo from '@/components/molcules/PostSubInfo';
-import QuizHistoryList from '@/components/organisms/QuizHistoryList';
 import UserProfileHeader from '@/components/organisms/UserProfileHeader';
+import useMecaHistory from '@/hooks/meca/useMecaHistory';
 import useUser from '@/hooks/user/useUser';
 import { ssrAspect } from '@/libs/renderAspect';
-import queryKey from '@/query/queryKey';
 import { TextCaption } from '@/styles/common';
 import { Devide, PostSection } from '@/styles/layout';
 import { PRIVATE_SSR_CDN_CACHE_VALUE } from '@/utils/constants';
+
+const QuizHistoryList = dynamic(() => import('@/components/organisms/QuizHistoryList'));
 
 export interface UserPageProps {
   memberId: string;
@@ -66,13 +67,7 @@ export const getServerSideProps: GetServerSideProps = ssrAspect(async (context, 
   if (!memberId) {
     throw { message: '회원정보가 존재하지 않음' };
   }
-  await queryClient.prefetchInfiniteQuery(
-    [queryKey.history, memberId],
-    () => cardHistoryApi.getHistories({ id: memberId, resourceType: 'members' }),
-    {
-      getNextPageParam: (lastPage) => lastPage.hasNext ?? undefined,
-    },
-  );
+  await useMecaHistory.prefetchInfiniteQuery('members', memberId, queryClient);
   context.res.setHeader('Cache-Control', PRIVATE_SSR_CDN_CACHE_VALUE);
   return {
     memberId,
