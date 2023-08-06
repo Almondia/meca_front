2d26606c5307a1f9d8703cba058705e0fa9e69d2
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-08-06 18:26:48 +0900

    feat: 3종류 CategoryList hooks 전략패턴화

--- pages/categories/me/[memberId].tsx
+++ pages/categories/me/[memberId].tsx
@@ -1,18 +1,18 @@
 /* eslint-disable @typescript-eslint/no-throw-literal */
 import { GetServerSideProps } from 'next';
 
-import categoryApi from '@/apis/categoryApi';
+import { useRecoilValue } from 'recoil';
+
+import { hasAuthState } from '@/atoms/common';
 import PageTitle from '@/components/atoms/PageTitle';
 import Selection from '@/components/atoms/Selection';
 import AuthPageProvider from '@/components/common/AuthPageProvider';
 import MetaHead from '@/components/common/MetaHead';
 import CategoryControl from '@/components/organisms/CategoryControl';
 import CategoryList from '@/components/organisms/CategoryList';
-import useMyCategory from '@/hooks/category/useMyCategory';
-import useMyRecommendedCategory from '@/hooks/category/useMyRecommendedCategory';
+import useCategoryList, { CategoryListFetcherKey } from '@/hooks/category/useCategoryList';
 import useQueryRouter from '@/hooks/useQueryRouter';
 import { ssrAspect } from '@/libs/renderAspect';
-import queryKey from '@/query/queryKey';
 import { Devide, ListSection } from '@/styles/layout';
 import { PRIVATE_SSR_CDN_CACHE_VALUE } from '@/utils/constants';
 
@@ -23,12 +23,12 @@ interface CategoryProps {
 const RECOMMEND_QUERY = { recommended: 'true' } as const;
 
 const Category = ({ isRecommendedRequest }: CategoryProps) => {
+  const hasAuth = useRecoilValue(hasAuthState);
   const { recommended, replaceWithQuery } = useQueryRouter<'recommended'>(isRecommendedRequest ? RECOMMEND_QUERY : {});
-  const myCategoryHook = useMyCategory(!recommended);
-  const myRecommendedCategoryHook = useMyRecommendedCategory(!!recommended);
-  const { categoryList, fetchNextPage, hasNextPage, query, changeSearchQuery, isEmpty } = recommended
-    ? myRecommendedCategoryHook
-    : myCategoryHook;
+  const { categoryList, isEmpty, hasNextPage, fetchNextPage, query, changeSearchQuery } = useCategoryList(
+    recommended ? 'recommended' : 'me',
+    !hasAuth,
+  );
   return (
     <AuthPageProvider>
       <MetaHead title="내 카테고리 목록" description="로그인 후 이용할 수 있어요!" />
@@ -58,16 +58,10 @@ const Category = ({ isRecommendedRequest }: CategoryProps) => {
 export const getServerSideProps: GetServerSideProps = ssrAspect(async (context, queryClient) => {
   const recommended = context?.query?.recommended;
   const isRecommendedRequest = recommended === 'true';
-  const categoriesKey = isRecommendedRequest
-    ? [queryKey.categories, 'me', 'recommended', '']
-    : [queryKey.categories, 'me', ''];
-  const getCategoryQueryFn = isRecommendedRequest
-    ? categoryApi.getMyRecommendedCategoryList
-    : categoryApi.getMyCategoryList;
-  await queryClient.prefetchInfiniteQuery(categoriesKey, async () => getCategoryQueryFn({}), {
-    getNextPageParam: (lastPage) => lastPage.hasNext ?? undefined,
-  });
-  queryClient.getQueryData(categoriesKey) && context.res.setHeader('Cache-Control', PRIVATE_SSR_CDN_CACHE_VALUE);
+  const queryKey: CategoryListFetcherKey = isRecommendedRequest ? 'recommended' : 'me';
+  await useCategoryList.prefetchInfiniteQuery(queryKey, queryClient);
+  !useCategoryList.isEmpty(queryKey, queryClient) &&
+    context.res.setHeader('Cache-Control', PRIVATE_SSR_CDN_CACHE_VALUE);
   return { isRecommendedRequest };
 });
 
