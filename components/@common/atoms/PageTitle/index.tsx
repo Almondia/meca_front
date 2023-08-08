e83f46d2760997fd60cc18fa38bf8714efad233e
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-08-08 09:50:50 +0900

    feat: PageTitle h 태그 수정 및 story 추가

--- components/@common/atoms/PageTitle/index.tsx
+++ components/@common/atoms/PageTitle/index.tsx
@@ -2,11 +2,15 @@ import styled from 'styled-components';
 
 const PageTitleWrapper = styled.div`
   margin-bottom: 30px;
+  h2 {
+    font-size: 2rem;
+    line-height: 2.375rem;
+  }
 `;
 
 const PageTitle = ({ children }: { children: React.ReactNode }) => (
   <PageTitleWrapper>
-    <h4>{children}</h4>
+    <h2>{children}</h2>
   </PageTitleWrapper>
 );
 
