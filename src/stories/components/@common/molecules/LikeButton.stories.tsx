2ea54ba3e99ca6baa68bc32cfbcffca76e62d9f0
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-08-16 18:56:53 +0900

    fix: Category LikeButton 무분별한 클릭으로 api 결과 반영 시 색상 변경이 안되는 문제 처리

--- src/stories/components/@common/molecules/LikeButton.stories.tsx
+++ src/stories/components/@common/molecules/LikeButton.stories.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import LikeButton from '@/components/@common/molecules/LikeButton';
@@ -10,9 +12,26 @@ export default {
   },
 } as ComponentMeta<typeof LikeButton>;
 
-const Template: ComponentStory<typeof LikeButton> = (args) => <LikeButton {...args} />;
+const Template: ComponentStory<typeof LikeButton> = ({ likeCount, disabled = false, defaultActiveState = true }) => {
+  const [active, setActive] = useState<boolean>(defaultActiveState);
+  const handleClick = () => {
+    setActive((prev) => !prev);
+    return !active;
+  };
+  return (
+    <LikeButton
+      defaultActiveState={defaultActiveState}
+      disabled={disabled}
+      likeCount={active ? likeCount + 1 : likeCount}
+      onClick={handleClick}
+      buttonName="name"
+    />
+  );
+};
 
 export const Default = Template.bind({});
 Default.args = {
   likeCount: 12,
+  disabled: false,
+  defaultActiveState: true,
 };
