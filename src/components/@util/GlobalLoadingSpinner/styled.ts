5083e8b9b5163987c96d1f9035465924af26569c
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-08-17 15:31:38 +0900

    feat: GlobalLoadingSpinner UI 식별 지연 처리 추가

--- src/components/@util/GlobalLoadingSpinner/styled.ts
+++ src/components/@util/GlobalLoadingSpinner/styled.ts
@@ -2,14 +2,14 @@ import styled from 'styled-components';
 
 import { FlexCenter } from '@/styles/layout';
 
-export const GlobalLoadingSpinnerWrapper = styled.div<{ isVisible: boolean }>`
+export const GlobalLoadingSpinnerWrapper = styled.div<{ isVisible: boolean; isDeferred: boolean }>`
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
   z-index: 100;
-  opacity: 0.6;
+  opacity: ${(props) => (props.isDeferred ? 0.6 : 0)};
   display: ${(props) => (props.isVisible ? 'inherit' : 'none')};
   background-color: black;
   & > div {
