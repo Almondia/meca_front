bb32cdc3650b6eed7606692ac2dbdc1b4f171445
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-08-27 16:39:35 +0900

    fix: GlobalLoadSpinner clean up

--- src/components/@util/GlobalLoadingSpinner/index.tsx
+++ src/components/@util/GlobalLoadingSpinner/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 import { useEffect, useState } from 'react';
 
 import { useIsMutating } from '@tanstack/react-query';
@@ -13,12 +14,13 @@ const GlobalLoadingSpinner = () => {
   const isMutating = useIsMutating({ mutationKey: ['default'], exact: true });
   const isLoading = !!(isMutating || isGlobalLoading);
   const [isDeferred, setIsDeferred] = useState(false);
-  const debouncedSetDeferred = useDebounce(() => setIsDeferred(true), 200);
+  const debouncedSetDeferred = useDebounce(setIsDeferred, 200);
   useEffect(() => {
     if (isLoading) {
-      debouncedSetDeferred();
-    } else {
-      setIsDeferred(false);
+      debouncedSetDeferred(true);
+      return () => {
+        debouncedSetDeferred(false);
+      };
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isLoading]);
