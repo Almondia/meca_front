baa5b829c1ecfa0865a2eb1b297d104893782e3b
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-09-05 14:45:34 +0900

    fix: fix missing quizCount memo dependencies

--- src/hooks/useCount.ts
+++ src/hooks/useCount.ts
@@ -3,13 +3,16 @@ import { useCallback, useState } from 'react';
 const useIncrease = (defaultNum: number, startValue: number, endValue: number) => {
   const [number, setNumber] = useState<number>(defaultNum);
 
-  const increaseNumber = (isIncrease: boolean) => {
-    if (isIncrease) {
-      number < endValue && setNumber(number + 1);
-      return;
-    }
-    number > startValue && setNumber(number - 1);
-  };
+  const increaseNumber = useCallback(
+    (isIncrease: boolean) => {
+      if (isIncrease) {
+        setNumber((prev) => (prev < endValue ? prev + 1 : prev));
+        return;
+      }
+      setNumber((prev) => (prev > startValue ? prev - 1 : prev));
+    },
+    [startValue, endValue],
+  );
 
   const resetNumber = useCallback(() => {
     setNumber(defaultNum);
